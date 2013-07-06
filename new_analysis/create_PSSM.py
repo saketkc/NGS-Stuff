b596a7eb985d42db2f39037eb02e79cf72185083
Saket Choudhary <saketkc@gmail.com>
2013-07-07 00:16:17 +0530

    lots of fixes

--- new_analysis/create_PSSM.py
+++ new_analysis/create_PSSM.py
@@ -7,14 +7,13 @@ class GetPSSM:
         self.fastq_format = fastq_format
         self.is_casava = False
         self.nucleotides=['A','G','T','C','N']
-        self.per_base_count = {i:0 for i in self.nucleotides}
+        self.per_base_count = {}
         self.read_wise_motif_count={}
         self.read_wise_motif_count = {}
         self.records = None
         self.casava_regexp = re.compile('@.* [^:]*:N:[^:]*:')
         self.fails_casava = re.compile('@.* [^:]*:Y:[^:]*:')
-
-         assert fastq_format in ['fastq', 'fastq-sanger', 'fastq-illumina', 'fastq-solexa']
+        assert fastq_format in ['fastq', 'fastq-sanger', 'fastq-illumina', 'fastq-solexa']
 
     def run_index( sel):
         self.fastq_index = SeqIO.index(self.filelocation, self.fastq_format)
@@ -26,26 +25,27 @@ class GetPSSM:
         self.records = [rec for rec in SeqIO.parse(self.filelocation, self.fastq_format)]
         return True
 
-    def create_motif_regex(motif):
+    def create_motif_regex(self, motif):
         return re.compile(motif)
 
     def get_read_wise_motif_content( self, per_base_content=True ):
         for i,rec in enumerate(self.records):
             motifs_count = {i+j:0 for i in self.nucleotides for j in self.nucleotides }
             if per_base_content:
-                self.per_base_count = {i:rec.tostring().count(i) for i in self.nucleotides}
+                base_count = {n:rec.seq.tostring().count(n) for n in self.nucleotides}
+                self.per_base_count[i] = base_count
             position = 0
             for motif in motifs_count.keys():
                 position=0
                 regexp = self.create_motif_regex(motif)
                 while True:
-                    result = regexp.search(rec.tostring(), position)
+                    result = regexp.search(rec.seq.tostring(), position)
                     if result is None:
                         break
                     else:
                         position+=1
                         motifs_count[motif]+=1
-            self.read_wise_motif_count[i]= motifs_dict
+            self.read_wise_motif_count[i]= motifs_count
         return True
 
 
@@ -77,7 +77,12 @@ class GetPSSM:
 
 if __name__=="__main__":
     get_pssm = GetPSSM(sys.argv[1], "fastq")
-    get_pssm.run_index()
+    get_pssm.read_in_memory()
+    get_pssm.get_read_wise_motif_content()
+    print get_pssm.read_wise_motif_count
+    print get_pssm.per_base_count
+
+    #get_pssm.run_index()
 
 #    for keys in get_pssm.run_index():
  #       print keys
