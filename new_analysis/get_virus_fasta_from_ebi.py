e049fbb0fc274f728554b22ef29a68fd6e510c8a
Saket Choudhary <saketkc@gmail.com>
2013-07-09 03:09:41 +0530

    minors change

--- new_analysis/get_virus_fasta_from_ebi.py
+++ new_analysis/get_virus_fasta_from_ebi.py
@@ -2,17 +2,22 @@
 import urllib2
 import re
 txt_ftp_location = "ftp://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/viruses.txt"
-handle = open("virus.txt","r")
+handle = open("papillomavirus.txt","r")
 response = handle.read()
+handle.close()
 project_ids = [line for line in response.splitlines()]
-outputpath="/data1/Custom_Fasta/"
-
+outputpath="/home/saket/my-softwares/NGS-Stuff/new_analysis/fastas/"
+#project_ids=["X74475"]
 for id in project_ids:
     try:
         handle = urllib2.urlopen("http://www.ebi.ac.uk/ena/data/view/"+str(id)+"&display=fasta")
-        record = handle.read(handle)
-        handle.close()
-        if not record:
+#        print handle.geturl()
+        record = handle.read()
+#        print type(record)
+#        handle.close()
+#        print record
+        if record=="":
+            print record
             print "Something not ok with : "+str(id)
         else:
             fasta=open(outputpath+str(id)+".fa","w")
--- new_analysis/nucleotide_content.py
+++ new_analysis/nucleotide_content.py
@@ -2,11 +2,11 @@
 import re,sys
 from Bio import SeqIO
 input_fasta = sys.argv[1]
-nucleotides=['A','T','G','C']
+nucleotides=['A','T','G','C','N']
 motifs_count = {i+j:0 for i in nucleotides for j in nucleotides}
 for rec in SeqIO.parse(input_fasta,"fasta"):
-    position = 0
     for motif in motifs_count.keys():
+        position = 0
         regexp = re.compile(motif)
         while True:
             matches = regexp.search(rec.seq.tostring(), position)
@@ -14,7 +14,7 @@ for rec in SeqIO.parse(input_fasta,"fasta"):
                 break
             position= matches.start() + 1
             motifs_count[motif]+=1
-
+#print motifs_count
 all_matrix = [i+j for i in nucleotides for j in nucleotides]
 row1 ="A\t"
 row2 ="T\t"
@@ -23,6 +23,7 @@ row4 = "C\t"
 row5 = "N\t"
 row0 = "\tA\tT\tG\tC\tN"
 row_A = [motifs_count['A'+str(i)] for i in nucleotides]
+print row_A
 sum_A =sum(row_A)*1.0
 row_T = [motifs_count['T'+str(i)] for i in nucleotides]
 sum_T =sum(row_T)*1.0
@@ -32,13 +33,31 @@ row_C = [motifs_count['C'+str(i)] for i in nucleotides]
 sum_C =sum(row_C)*1.0
 row_N = [motifs_count['N'+str(i)] for i in nucleotides]
 sum_N =sum(row_N)*1.0
+try:
 
-row1  += ("\t").join("%0.3f" % (r/sum_A) for r in row_A)
-row2  += ("\t").join("%0.3f" % (r/sum_T) for r in row_T)
-row3  += ("\t").join("%0.3f" % (r/sum_G) for r in row_G)
-row4  += ("\t").join("%0.3f" % (r/sum_C) for r in row_C)
+    row1  += ("\t").join("%0.3f" % (r/sum_A) for r in row_A)
+except ZeroDivisionError:
+    row1 += ("\t").join("0" for r in row_A)
 
-row5  += ("\t").join(str(r/sum_N) for r in row_N)
+try:
+    row2  += ("\t").join("%0.3f" % (r/sum_T) for r in row_T)
+except ZeroDivisionError:
+    row2 += ("\t").join("0" for r in row_T)
+try:
+    row3  += ("\t").join("%0.3f" % (r/sum_G) for r in row_G)
+except ZeroDivisionError:
+    row3 += ("\t").join("0" for r in row_G)
+try:
+    row4  += ("\t").join("%0.3f" % (r/sum_C) for r in row_C)
+except ZeroDivisionError:
+
+    row4  += ("\t").join("0" for r in row_C)
+try:
+    row5  += ("\t").join("%0.3f" % (r/sum_N) for r in row_N)
+
+except ZeroDivisionError:
+
+    row5  += ("\t").join("0" for r in row_N)
 print row0
 print row1
 print row2
